794b23686f5f3e481930e285b5886076bd4ff70b
MathanKumar K <91076873+mathanroot@users.noreply.github.com>
2025-08-04 14:30:12 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,10 +3,12 @@ const app = express();
 const port = 9090;
 
 app.get('/', (req, res) => res.send('Hello from Docker + GitHub Actions!'));
+app.get('/', (req, res) => res.send('HELLO I AM MATHAN'));
 app.listen(port, () => console.log(`App listening at http://localhost:${port}`));
 app.listen(80, () => {
   console.log("Server running on port 80");
 });
+
 
 
 
