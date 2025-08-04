1f203f2d4ebadd8c0594ea6c88665d3d563d7358
MathanKumar K <91076873+mathanroot@users.noreply.github.com>
2025-08-04 14:37:57 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,12 +2,12 @@ const express = require('express');
 const app = express();
 const port = 9090;
 
-app.get('/', (req, res) => res.send('Hello from Docker + GitHub Actions!'));
-app.get('/', (req, res) => res.send('HELLO I AM MATHAN'));
+app.get('/', (req, res) => res.send('Hello from Docker + GitHub Actions! HI I AM MATHAN'));
 app.listen(port, () => console.log(`App listening at http://localhost:${port}`));
 app.listen(80, () => {
   console.log("Server running on port 80");
 });
+
 
 
 
